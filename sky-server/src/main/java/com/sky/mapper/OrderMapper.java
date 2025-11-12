32e5288c330ac1e0c06a3f5311f476d4b6525c39
lixiang3 <1751631880@qq.com>
2025-11-12 21:27:35 +0800

    查询指定时间区间内的销量排名top10

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -83,4 +84,12 @@ public interface OrderMapper {
      * @param map
      */
     Integer OrdercountByMap(Map map);
+
+    /**
+     * 查询指定时间区间内的销量排名top10
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime beginTime, LocalDateTime endTime);
 }
